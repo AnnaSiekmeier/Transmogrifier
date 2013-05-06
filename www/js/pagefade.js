1ec702aba288e116d6b9b41e7efd717ffea13996
AnnaSiekmeier <siek09@gmail.com>
2013-05-06 09:53:11 -0500

    May6 update

--- www/js/pagefade.js
+++ www/js/pagefade.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 	
 	$("body").css("display", "none");
 
-    $("body").fadeIn(1000);
+    $("body").fadeIn(500);
     
 	$("a.transition").click(function(event){
 		event.preventDefault();
 		linkLocation = this.href;
-		$("body").fadeOut(500, redirectPage);		
+		$("body").fadeOut(250, redirectPage);		
 	});
 		
 	function redirectPage() {
